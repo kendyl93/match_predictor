224c10d02e78484fbdb58792ad48106102c453f4
Pawel Stanecki <p.stanecki93@gmail.com>
2025-07-05 19:46:34 +0200

    Created requirements.txt

--- run_fetch.py
+++ run_fetch.py
@@ -1,12 +1,18 @@
+from dotenv import load_dotenv
+import os
+
 from scripts.fetch_api import fetch_matches
 from scripts.parse_matches import parse_matches
 from scripts.save_to_csv import save_to_csv
 
+load_dotenv()
+token = os.getenv("API_TOKEN")
+
 def main():
-    token = ''
     data = fetch_matches(token)
     parsed_data = parse_matches(data)
     save_to_csv(parsed_data, "matches.csv")
+    print(f"Fetched and saved {len(parsed_data)} games.")
 
 if __name__ == "__main__":
     main()
